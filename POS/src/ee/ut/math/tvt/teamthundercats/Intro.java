191192baf2927b718781c8230c99c7bef18150e4
Jaga007 <gimbutas@live.com>
2013-10-15 20:15:59 +0300

    Homework 3

---
+++ POS/src/ee/ut/math/tvt/teamthundercats/Intro.java
@@ -0,0 +1,44 @@
+package ee.ut.math.tvt.teamthundercats;
+
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+@SuppressWarnings("serial")
+public class Intro extends JPanel {
+
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Intro team ThunderCats");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add content to the window.
+        frame.add(new IntroUI());
+       
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+        //Turn off metal's use of bold fonts
+            UIManager.put("swing.boldMetal", Boolean.FALSE);
+                 
+        createAndShowGUI();
+            }
+        });
+    }
+
+
+}
