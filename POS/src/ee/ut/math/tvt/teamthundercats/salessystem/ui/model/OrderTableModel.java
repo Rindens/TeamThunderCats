6c469f18181411e6d38bcf08031afdc4f40aa084
Rindens <jana.levitina@gmail.com>
2013-11-08 16:28:45 +0200

    Update OrderTableModel.java

--- POS/src/ee/ut/math/tvt/teamthundercats/salessystem/ui/model/OrderTableModel.java
+++ POS/src/ee/ut/math/tvt/teamthundercats/salessystem/ui/model/OrderTableModel.java
@@ -1,80 +1,153 @@
 package ee.ut.math.tvt.teamthundercats.salessystem.ui.model;
 
-import java.util.NoSuchElementException;
-
-import org.apache.log4j.Logger;
+ 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
 import ee.ut.math.tvt.teamthundercats.salessystem.domain.data.Order;
-import ee.ut.math.tvt.teamthundercats.salessystem.domain.data.SoldItem;
-import ee.ut.math.tvt.teamthundercats.salessystem.domain.data.StockItem;
-import ee.ut.math.tvt.teamthundercats.salessystem.ui.SalesSystemUI;
-import ee.ut.math.tvt.teamthundercats.salessystem.ui.panels.PurchaseItemPanel;
 
 
-/**
- * Purchase history details model. 
- */
-public class OrderTableModel extends SalesSystemTableModel<Order> {
-	private static final long serialVersionUID = 1L;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    private PurchaseItemPanel.ComboListener comboListener;
-	
-	private static final Logger log = Logger.getLogger(OrderTableModel.class);
-	
-	
-	public OrderTableModel() {
-		super(new String[] { "Id", "Date", "Price"});
+
+
+public class OrderTableModel extends JPanel {
+	public static JTable getTable() {
+		return table;
 	}
 
-	@Override
-	protected Object getColumnValue(Order item, int columnIndex) {
-		switch (columnIndex) {
-		case 0:
-			return item.getOrderId();
-		case 1:
-			return item.getDateTime();
-		case 2:
-			return item.getTotalPrice();
-
-		}
-		throw new IllegalArgumentException("Column index out of range");
+	public void setTable(JTable table) {
+		this.table = table;
 	}
 
-	@Override
-	public String toString() {
-		final StringBuffer buffer = new StringBuffer();
-
-		for (int i = 0; i < headers.length; i++)
-			buffer.append(headers[i] + "\t");
-		buffer.append("\n");
-
-		for (final Order item : rows) {
-			buffer.append(item.getOrderId() + "\t");
-			buffer.append(item.getDateTime() + "\t");
-			buffer.append(item.getTotalPrice() + "\t");
-			buffer.append("\n");
-		}
-
-		return buffer.toString();
-	}
-	
-
-
-	
-	
+	public static JTable table = new JTable();
+    private boolean DEBUG = false;
+ 
+    public OrderTableModel() {
+        super(new GridLayout(1,0));
+ 
+        table = new JTable(new MyTableModel());
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
+ 
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+ 
+        //Add the scroll pane to this panel.
+        add(scrollPane);
+    }
+ 
+    class MyTableModel extends AbstractTableModel {
+        private String[] columnNames = {"ID",
+                                        "Date",
+                                        "Total price"};
+		
+        private Object[][] data = {
+        {"0", new Date(),
+         new Double(0.0)},
+        };
+ 
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+ 
+        public int getRowCount() {
+            return data.length;
+        }
+ 
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+ 
+        public Object getValueAt(int row, int col) {
+            return data[row][col];
+        }
+ 
+        /*
+         * JTable uses this method to determine the default renderer/
+         * editor for each cell.  If we didn't implement this method,
+         * then the last column would contain text ("true"/"false"),
+         * rather than a check box.
+         */
+        public Class getColumnClass(int c) {
+            return getValueAt(0, c).getClass();
+        }
+ 
+        /*
+         * Don't need to implement this method unless your table's
+         * editable.
+         */
+        public boolean isCellEditable(int row, int col) {
+            //Note that the data/cell address is constant,
+            //no matter where the cell appears on screen.
+            if (col < 2) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+ 
+        /*
+         * Don't need to implement this method unless your table's
+         * data can change.
+         */
+        public void setValueAt(Order value, int row, int col) {
+            if (DEBUG) {
+                System.out.println("Setting value at " + row + "," + col
+                                   + " to " + value
+                                   + " (an instance of "
+                                   + value.getClass() + ")");
+            }
+ 
+            data[row][col] = value.getOrderId();
+            fireTableCellUpdated(row, col);
+ 
+            if (DEBUG) {
+                System.out.println("New value of data:");
+                printDebugData();
+            }
+        }
+ 
+        private void printDebugData() {
+            int numRows = getRowCount();
+            int numCols = getColumnCount();
+ 
+            for (int i=0; i < numRows; i++) {
+                System.out.print("    row " + i + ":");
+                for (int j=0; j < numCols; j++) {
+                    System.out.print("  " + data[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println("--------------------------");
+        }
+    }
+ 
     /**
-     * Add new StockItem to table.
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
      */
-    public void addItem(final Order item) {
-        
-        
-        rows.add(item);
-       
-        fireTableDataChanged();
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("OrderTableModel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Create and set up the content pane.
+        OrderTableModel newContentPane = new OrderTableModel();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
 
-	public void setComboListener(PurchaseItemPanel.ComboListener comboListener) {
-		// TODO Auto-generated method stub
-		
-	}
 }
